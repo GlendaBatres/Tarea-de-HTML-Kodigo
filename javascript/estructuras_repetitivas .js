ff7c6790155eb338ed7cd168e7c6eaa4f8027f9b
GlendaBatres <glenda.xiomara.batres@clases.edu.sv>
2026-05-04 20:03:20 -0600

    Landigpage de recetas

---
+++ javascript/estructuras_repetitivas .js
@@ -0,0 +1,25 @@
+// Repetir un bloque de código (bucles)
+
+// ciclo for
+// imprimiendo los numeros del 1 al 10
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log("------uso del WHILE-------");
+// ciclo while -> cuando no sabemos hasta donde va llegar el bucle
+let j = 0;
+while (j < 10) {
+    console.log(j);
+    j++;
+}
+console.log("------uso del DO-WHILE-------");
+// ciclo do-while -> se ejecuta al menos una vez
+let k = 11;
+do {
+    console.log(k);
+    k++;
+} while (k < 10);
+
+
